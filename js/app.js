2461fb4da29524ef39ef0dd61be3dc273ba99b71
Juan Miguel Rodriguez <juanmiguelrodriguezortiz@gmail.com>
2024-03-19 10:13:00 -0500

    repositorio de mi portafolio

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const formularioContacto = document.getElementById('formulario-contacto')
+const userName = document.getElementById('userName')
+const empresaName = document.getElementById('empresaName')
+const userEmail = document.getElementById('userEmail')
+const message = document.getElementById('message')
+const button = document.getElementById('button')
+
+console.log()
+
+formularioContacto.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
